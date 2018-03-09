e72fe41df9b2af65fef3a82fb434877de228ed22
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2018-03-08 23:59:30 -0500

    simplify check

--- habstranslate/__main__.py
+++ habstranslate/__main__.py
@@ -1,6 +1,7 @@
 import json
 import re
 from urllib.parse import urlparse
+from pprint import pprint
 
 import langdetect
 import praw
@@ -9,8 +10,6 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 from requests import Request
 
-from .cache import LRUContext
-
 ua = UserAgent()
 
 _RE_SPLIT = re.compile(r"[\t\n\r\s]+")
@@ -18,6 +17,7 @@ _BLACKLIST = [
     "youtube.com",
     "youtu.be",
     "streamable.com",
+    "imgur.com",
 ]
 
 
@@ -25,6 +25,10 @@ def is_blacklisted(url):
     url = urlparse(url)
     domain = url.netloc.lower()
     return any(site in domain for site in _BLACKLIST)
+
+
+def printf(fmt, *args, **kwargs):
+    print(fmt.format(*args, **kwargs))
 
 
 def quote(url):
@@ -40,6 +44,10 @@ def process_submission(submission):
     }
     resp = requests.get(submission.url, headers=headers)
     resp.raise_for_status()
+    pprint(resp.headers)
+    if "text/html" not in resp.headers['content-type']:
+        return
+
     doc = BeautifulSoup(resp.text, "html.parser")
     words = []
     for elem in doc.find_all('a'):
@@ -72,29 +80,32 @@ def main():
     with open('config.json') as f:
         config = json.load(f)
 
+        username = config['username']
+
         reddit = praw.Reddit(client_id=config['client_id'],
                              client_secret=config['client_secret'],
-                             username=config['username'],
+                             username=username,
                              password=config['password'],
                              user_agent=config['user_agent'])
 
         subreddit = reddit.subreddit(config['subreddit'])
 
-        with LRUContext("lru_cache.txt", 150) as cache:
-            try:
-                for submission in subreddit.stream.submissions():
-                    if cache.has(submission.id):
-                        continue
-                    cache.add(submission.id)
-                    print(submission.title)
-                    if not submission.is_self:
-                        try:
-                            process_submission(submission)
-                        except Exception as e:
-                            raise
-            except KeyboardInterrupt:
-                print()
-                pass
+        for submission in subreddit.stream.submissions():
+            if not submission.is_self:  # link submission
+                # make sure we haven't replied already
+                process = True
+                for comment in submission.comments:
+                    if comment.is_root and comment.author.name.lower() == username.lower():
+                        process = False
+                        break
+                if process:
+                    printf("Processing '{}', repliying with translated link", submission.title)
+                    try:
+                        process_submission(submission)
+                    except Exception as e:
+                        raise
+                else:
+                    printf("Processing '{}', skipping because it has already been replied to", submission.title)
 
 
 if __name__ == '__main__':
